9ec6704f897966a1b77cb556fbf1c127e35695de
Chinmay <chinz.sandur07@gmail.com>
2024-02-24 16:00:05 +0530

    add logic to number of words and characters

--- 02/src/components/TextForm.jsx
+++ 02/src/components/TextForm.jsx
@@ -3,27 +3,31 @@ import Form from 'react-bootstrap/Form';
 function TextForm(props) {
     const [text, setText] = useState("Enter text here");
     function clickHandler() {
-        console.log("Uppercase was Clicked");
-        setText("You have clicked on clickHandler");
+        console.log("Uppercase was Clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
     }
     function onChangeHandler(e) {
         console.log("OnChange");
         setText(e.target.value)
     }
     return (
-        <div>
+        <>
 
-            <Form>
-                <h2>{props.heading}</h2>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" id='mybox' >
-                    <Form.Control as="textarea" rows={8} value={text} onChange={onChangeHandler} />
-                </Form.Group>
-            </Form>
-            <button className='btn btn-primary' onClick={clickHandler}>Convert to UpperCase</button>
-
-
-
-        </div>
+            <div className='container'>
+                <Form>
+                    <h2>{props.heading}</h2>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" id='mybox' >
+                        <Form.Control as="textarea" rows={8} value={text} onChange={onChangeHandler} />
+                    </Form.Group>
+                </Form>
+                <button className='btn btn-primary' onClick={clickHandler}>Convert to UpperCase</button>
+            </div>
+            <div className='container my-3'>
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words and  and {text.length} characters</p>
+            </div>
+        </>
     )
 }
 
