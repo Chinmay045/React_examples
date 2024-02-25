342fdd16761a7465eba74c5a8a2382da44842b57
Chinmay <chinz.sandur07@gmail.com>
2024-02-25 16:04:51 +0530

    add alert messages

--- 02/src/App.js
+++ 02/src/App.js
@@ -3,26 +3,38 @@ import './App.css';
 import Navba from './components/Navba';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import Alerts from './components/Alerts.jsx';
 import { useState } from 'react';
 
 
 function App() {
   const [darkMode, setDarkMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  function showAlert(message, type) {
+    setAlert({
+      msg: message,
+      type: type
+    });
+  }
 
   function toggleMode() {
     if (darkMode === 'light') {
       setDarkMode('dark')
       document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "Success")
     }
     else {
       setDarkMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success")
 
     }
   }
   return (
     <div>
       <Navba title={"TextUtils"} about="About Us" mode={darkMode} toggleMode={toggleMode} />
+      <Alerts alert={alert} />
       <div className="container">
         <TextForm heading={"Enter the text to analyze"} />
       </div>
---
+++ 02/src/components/Alerts.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+
+function Alerts(props) {
+    const [show, setShow] = useState(true);
+
+    function capitalize(str) {
+        const lower = str.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
+
+    if (show) {
+        return (
+            props.alert && <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+                <Alert.Heading></Alert.Heading>
+                <p>
+                    <strong>{capitalize(props.alert.type)}</strong> :{props.alert.msg}
+                </p>
+            </Alert>
+        );
+    }
+    return
+    //  <Button onClick={() => setShow(true)}></Button>;
+}
+
+export default Alerts;
